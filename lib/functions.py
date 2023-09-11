1a49000d94913d8bf90ca1b91ee007c785f2d603
Vanessa Jesik <vajesik@gmail.com>
2023-09-11 15:11:42 -0600

    completed functions

--- lib/functions.py
+++ lib/functions.py
@@ -1,16 +1,23 @@
 #!/usr/bin/env python3
 
+from cgi import print_arguments
+
+
 def greet_programmer():
-    pass
+    print("Hello, programmer!")
+
 
 def greet(name):
-    pass
+    print(f"Hello, {name}!")
+
 
 def greet_with_default(name="programmer"):
-    pass
+    print(f"Hello, {name}!")
+
 
 def add(num1, num2):
-    pass
+    return num1 + num2
+
 
 def halve(number):
-    pass
+    return number / 2
